955aa76f6e859def907c432d35bce5a8eb222409
MostafaAmrHamed <mostafaamro5619@gmail.com>
2022-05-04 03:11:55 +0200

    exported the main colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          1: "#EC3642",
+          2: "#F9F9F9",
+        },
+        text: {
+          1: "#313131",
+          2: "#B1B0B0",
+        },
+      },
+    },
   },
   plugins: [],
 };
