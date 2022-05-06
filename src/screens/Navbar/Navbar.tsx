df59c052f3213e066f8887e3dfbf6b744ce2c7d2
MostafaAmrHamed <mostafaamro5619@gmail.com>
2022-05-07 00:52:56 +0200

    feat(navbar): Designed mobile navbar

--- src/screens/Navbar/Navbar.tsx
+++ src/screens/Navbar/Navbar.tsx
@@ -1,9 +1,11 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { FiMenu } from "react-icons/fi";
 const Navbar = () => {
+  const [navToggle, setNavToggle] = useState(false);
   return (
     <div className="container mx-auto mt-5">
-      <div className="flex justify-between lg:mx-5 xl:mx-0">
+      {/* Desktop Navbar */}
+      <div className="md:flex justify-between mx-5 xl:mx-0 hidden">
         <div>
           <h1 className="text-2xl text-primary-1 font-semibold">Fast Care</h1>
         </div>
@@ -26,6 +28,40 @@ const Navbar = () => {
             </li>
           </ul>
         </div>
+      </div>
+      {/* Mobile Navbar */}
+      <div className="mx-5 md:hidden">
+        <nav className="flex justify-between">
+          <h1 className="text-2xl text-primary-1 font-semibold">Fast Care</h1>
+          <FiMenu
+            size={30}
+            className="cursor-pointer"
+            onClick={() => {
+              setNavToggle(!navToggle);
+            }}
+          />
+        </nav>
+        {navToggle && (
+          <div className="bg-primary-1 p-5 rounded-b-lg">
+            <ul className="space-y-5 text-primary-2">
+              <li className="cursor-pointer hover:pl-2 transition-all ease-in-out">
+                Home
+              </li>
+              <li className="cursor-pointer hover:pl-2 transition-all ease-in-out">
+                About
+              </li>
+              <li className="cursor-pointer hover:pl-2 transition-all ease-in-out">
+                Lost People
+              </li>
+              <li className="cursor-pointer hover:pl-2 transition-all ease-in-out">
+                Blood Bank
+              </li>
+              <li className="cursor-pointer hover:pl-2 transition-all ease-in-out">
+                Rooms
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </div>
   );
