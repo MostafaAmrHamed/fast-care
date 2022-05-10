19867e2421bd75543c95f935ea814204e93bb599
MostafaAmrHamed <mostafaamro5619@gmail.com>
2022-05-10 12:48:41 +0200

    feat(blood-bank): Added get your current location

--- src/screens/Blood-bank/BloodBank.tsx
+++ src/screens/Blood-bank/BloodBank.tsx
@@ -1,20 +1,36 @@
 import React, { useState } from "react";
 import { HiLocationMarker } from "react-icons/hi";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import {
+  MapContainer,
+  Marker,
+  Popup,
+  TileLayer,
+  useMapEvents,
+} from "react-leaflet";
 const BloodBank = () => {
   const [bloodType, setBloodType] = useState("");
+  const [position, setPosition] = useState(null);
+  const LocationMarker = () => {
+    const map = useMapEvents({
+      click() {
+        map.locate();
+      },
+      locationfound(e: any) {
+        setPosition(e.latlng);
+        map.flyTo(e.latlng, map.getZoom());
+      },
+    });
+
+    return position === null ? null : (
+      <Marker position={position}>
+        <Popup>You are here</Popup>
+      </Marker>
+    );
+  };
+
   return (
     <div className="container mx-auto mt-5">
-      <div className="flex flex-col md:flex-row gap-5 mx-2 md:mx-0 w-fit">
-        <button
-          className="bg-primary-1 text-base md:text-lg px-4 py-2 text-primary-2 flex items-center gap-2 rounded-lg"
-          onClick={() => {
-            console.log(bloodType);
-          }}
-        >
-          Get your current location
-          <HiLocationMarker size={30} />
-        </button>
+      <div className="flex flex-row gap-5 mx-2 md:mx-0 w-fit">
         <select
           value={bloodType}
           onChange={(e) => {
@@ -32,19 +48,20 @@ const BloodBank = () => {
           <option value="O+">O+</option>
           <option value="O-">O-</option>
         </select>
+        <button
+          className="bg-primary-1 text-base md:text-lg px-4 py-2 text-primary-2 flex items-center gap-2 rounded-lg"
+          onClick={() => {
+            console.log(bloodType);
+          }}
+        >
+          Search
+        </button>
       </div>
-      {/* <div className="text-center text-5xl font-bold flex justify-center items-center h-[300px] md:h-[400px] leading-normal">
-        <h1>
-          <span className="text-red-700">1</span> pint
-          <br /> can save <br />
-          <span className="text-red-700"> 3</span> lives
-        </h1>
-      </div> */}
       <div className="flex flex-col lg:flex-row items-center gap-10">
         <div className="p-2 bg-text-2 mt-5 rounded-md">
           <MapContainer
             className="h-[300px] w-[300px] md:h-[350px] md:w-[600px] lg:w-[450px] xl:w-[650px] xl:h-[450px]"
-            center={[51.505, -0.09]}
+            center={[30.04442, 31.235712]}
             zoom={13}
             scrollWheelZoom={false}
           >
@@ -52,12 +69,12 @@ const BloodBank = () => {
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[51.505, -0.09]}>
-              <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-              </Popup>
-            </Marker>
+            <LocationMarker />
           </MapContainer>
+          <p className="flex flex-col md:flex-row text-sm items-center justify-center mt-2 bg-primary-1 text-primary-2 text-center p-2 rounded-md">
+            Click on map to get your current location
+            <HiLocationMarker size={25} />
+          </p>
         </div>
         <div className="text-center text-5xl font-bold leading-normal">
           <h1>
