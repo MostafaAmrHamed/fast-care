631623a020c7c9486b7f6d093036c4e248b2b225
MostafaAmrHamed <mostafaamro5619@gmail.com>
2022-05-23 19:18:00 +0200

    feat(api): Routing each page and link with navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import BloodBank from "./screens/Blood-bank/BloodBank";
 import LostPeople from "./screens/Lost-people/LostPeople";
 import Navbar from "./screens/Navbar/Navbar";
+import Rooms from "./screens/Rooms/Rooms";
 
 function App() {
   return (
-    <div className="font-poppins px-2">
+    <div className="font-poppins px-2 py-4">
       <Navbar />
-      <LostPeople />
+      <Routes>
+        <Route path="/rooms" element={<Rooms />} />
+        <Route path="/lostPeople" element={<LostPeople />} />
+        <Route path="/bloodBank" element={<BloodBank />} />
+      </Routes>
     </div>
   );
 }
--- src/screens/Navbar/Navbar.tsx
+++ src/screens/Navbar/Navbar.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FiMenu } from "react-icons/fi";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 const Navbar = () => {
+  const location = useLocation();
   const [navToggle, setNavToggle] = useState(false);
   return (
     <div className="container mx-auto mt-5">
@@ -11,20 +13,33 @@ const Navbar = () => {
         </div>
         <div>
           <ul className="flex justify-between gap-5 text-lg text-text-2">
-            <li className="hover:text-primary-1 cursor-pointer transition-all ease-in-out">
-              Home
+            <li
+              className="hover:text-primary-1 cursor-pointer transition-all ease-in-out"
+              style={{ color: location.pathname === "/" ? "#EC3642" : "" }}
+            >
+              <Link to="/"> Home </Link>
             </li>
-            <li className="hover:text-primary-1 cursor-pointer transition-all ease-in-out">
-              About
+            <li
+              className="hover:text-primary-1 cursor-pointer transition-all ease-in-out"
+              style={{
+                color: location.pathname === "/lostPeople" ? "#EC3642" : "",
+              }}
+            >
+              <Link to="/lostPeople"> Lost People </Link>
             </li>
-            <li className="hover:text-primary-1 cursor-pointer transition-all ease-in-out">
-              Lost People
+            <li
+              className="hover:text-primary-1 cursor-pointer transition-all ease-in-out"
+              style={{
+                color: location.pathname === "/bloodBank" ? "#EC3642" : "",
+              }}
+            >
+              <Link to="/bloodBank"> Blood Bank </Link>
             </li>
-            <li className="hover:text-primary-1 cursor-pointer transition-all ease-in-out">
-              Blood Bank
-            </li>
-            <li className="hover:text-primary-1 cursor-pointer transition-all ease-in-out">
-              Rooms
+            <li
+              className="hover:text-primary-1 cursor-pointer transition-all ease-in-out"
+              style={{ color: location.pathname === "/rooms" ? "#EC3642" : "" }}
+            >
+              <Link to="/rooms"> Rooms </Link>
             </li>
           </ul>
         </div>
@@ -45,19 +60,16 @@ const Navbar = () => {
           <div className="bg-primary-1 p-5 rounded-b-lg">
             <ul className="space-y-5 text-primary-2">
               <li className="cursor-pointer hover:pl-2 transition-all ease-in-out">
-                Home
+                <Link to="/"> Home </Link>
               </li>
               <li className="cursor-pointer hover:pl-2 transition-all ease-in-out">
-                About
+                <Link to="/lostPeople"> Lost People </Link>
               </li>
               <li className="cursor-pointer hover:pl-2 transition-all ease-in-out">
-                Lost People
+                <Link to="/bloodBank"> Blood Bank </Link>
               </li>
               <li className="cursor-pointer hover:pl-2 transition-all ease-in-out">
-                Blood Bank
-              </li>
-              <li className="cursor-pointer hover:pl-2 transition-all ease-in-out">
-                Rooms
+                <Link to="/rooms"> Rooms </Link>
               </li>
             </ul>
           </div>
